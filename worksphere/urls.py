31c8a0738de8d30254870b9728f8f50f8107b640
Saeed <durrani.saeed1@gmail.com>
2024-07-09 22:10:30 -0400

    testurl

--- worksphere/urls.py
+++ worksphere/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from worksphere.views import test, hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hello/', hello_world, name='hello_world'),
 ]
