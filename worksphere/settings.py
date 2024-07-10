1a01b7186cccfc42de6922365b147e66733eda57
Saeed <durrani.saeed1@gmail.com>
2024-07-09 21:32:47 -0400

    first

--- worksphere/settings.py
+++ worksphere/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import dj_database_url
+import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +77,15 @@ WSGI_APPLICATION = 'worksphere.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
+
+# Configure static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 # Password validation
