17a06d282e0be1e4bf2af48614f180e6a1df66e8
Saeed <durrani.saeed1@gmail.com>
2024-07-11 19:31:33 -0400

    user model

--- worksphere/settings.py
+++ worksphere/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'worksphere',
 ]
 
-AUTH_USER_MODEL = 'worksphere.models.CustomUser'
+AUTH_USER_MODEL = 'worksphere.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
