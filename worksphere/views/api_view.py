cb67d345778ca76f257c457732ab58f55865ed39
Saeed <durrani.saeed1@gmail.com>
2024-07-14 16:47:07 -0400

    dummy

--- worksphere/views/api_view.py
+++ worksphere/views/api_view.py
@@ -1,5 +1,3 @@
-# api_view.py
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
